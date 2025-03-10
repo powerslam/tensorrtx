b092e6619c302568d1e6ad697bce7e06a0d75d43
powerslam <timsort@kookmin.ac.kr>
2025-03-10 14:37:17 +0900

    feat: morai를 위한 callback 함수 구현 및 조건부 subscribe

--- yolop/yolop_node.cpp
+++ yolop/yolop_node.cpp
@@ -5,27 +5,43 @@ YOLOPNode::YOLOPNode(): it(nh){
 
     this->yolop.read_engine_file(pkg_path + "/src/tensorrtx/yolop/build/yolop.trt");
 
-    this->res_pub = it.advertise("/detection_result", 1);
+    this->res_pub = it.advertise("/lane_segment", 1);
 
-    this->img_sub = nh.subscribe("/usb_cam/image_raw", 1, &YOLOPNode::imgCallback, this);
+    ros::param::get("~use_morai", this->use_morai);
+
+    if(!use_morai) {
+        this->img_sub = nh.subscribe("/image_jpeg/compressed", 1, &YOLOPNode::compressedImgCallback, this);
+    }
+    
+    else {
+        this->img_sub = nh.subscribe("/usb_cam/image_raw", 1, &YOLOPNode::imgCallback, this);
+    }
 }
 
 YOLOPNode::~YOLOPNode(){
     cv::destroyAllWindows();
 }
 
-void YOLOPNode::imgCallback(const sensor_msgs::ImageConstPtr msg) {
+void YOLOPNode::imgCallback(const sensor_msgs::ImageConstPtr& msg) {
     cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     cv::Mat img = cv_ptr->image;
+    this->publishYOLOPresult(img);
+}
 
-    yolop.set_img(img);
-    auto inf = yolop.inference();
-    yolop.visualization(inf);
+void YOLOPNode::compressedImgCallback(const sensor_msgs::CompressedImageConstPtr& msg) {
+    cv::Mat img = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_COLOR);
+    this->publishYOLOPresult(img);
+}
+
+void YOLOPNode::publishYOLOPresult(const cv::Mat& img){
+    this->yolop.set_img(img);
+    auto inf = this->yolop.inference();
+    this->yolop.visualization(inf);
 
     cv_bridge::CvImage bridge(
-        std_msgs::Header(), sensor_msgs::image_encodings::BGR8, yolop.get_img());
+        std_msgs::Header(), sensor_msgs::image_encodings::BGR8, this->yolop.get_img());
 
-    this->res_pub.publish(bridge.toImageMsg());
+    this->res_pub.publish(this->bridge.toImageMsg());
 }
 
 int main(int argc, char** argv) {
